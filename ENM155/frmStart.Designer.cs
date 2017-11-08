a411c44fbcdef03d867bf375dc961c98b14bfe8e
Hakim <hakimm@student.chalmers.se>
2017-11-08 13:36:22 +0100

    Add project files.

---
+++ ENM155/frmStart.Designer.cs
@@ -0,0 +1,369 @@
+﻿namespace ENM155
+{
+    partial class frmStart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtIndustri2 = new System.Windows.Forms.TextBox();
+            this.txtIndustri1 = new System.Windows.Forms.TextBox();
+            this.txtBastad2 = new System.Windows.Forms.TextBox();
+            this.txtBostad1 = new System.Windows.Forms.TextBox();
+            this.txtTrp = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chkBio = new System.Windows.Forms.CheckBox();
+            this.chkFossila = new System.Windows.Forms.CheckBox();
+            this.chkSpill = new System.Windows.Forms.CheckBox();
+            this.chkKarn = new System.Windows.Forms.CheckBox();
+            this.chkVatten = new System.Windows.Forms.CheckBox();
+            this.chkVind = new System.Windows.Forms.CheckBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.cboTime = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 726);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1325, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(909, 453);
+            this.panel1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtIndustri2);
+            this.groupBox1.Controls.Add(this.txtIndustri1);
+            this.groupBox1.Controls.Add(this.txtBastad2);
+            this.groupBox1.Controls.Add(this.txtBostad1);
+            this.groupBox1.Controls.Add(this.txtTrp);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.groupBox1.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 480);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(909, 229);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ENERGIANVÄNDNINGEN I OLIKA SEKTORER (TWh/År)";
+            // 
+            // txtIndustri2
+            // 
+            this.txtIndustri2.Location = new System.Drawing.Point(573, 184);
+            this.txtIndustri2.Name = "txtIndustri2";
+            this.txtIndustri2.Size = new System.Drawing.Size(100, 31);
+            this.txtIndustri2.TabIndex = 10;
+            this.txtIndustri2.Text = "49";
+            this.txtIndustri2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtIndustri1
+            // 
+            this.txtIndustri1.Location = new System.Drawing.Point(388, 184);
+            this.txtIndustri1.Name = "txtIndustri1";
+            this.txtIndustri1.Size = new System.Drawing.Size(100, 31);
+            this.txtIndustri1.TabIndex = 9;
+            this.txtIndustri1.Text = "91";
+            this.txtIndustri1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtBastad2
+            // 
+            this.txtBastad2.Location = new System.Drawing.Point(573, 135);
+            this.txtBastad2.Name = "txtBastad2";
+            this.txtBastad2.Size = new System.Drawing.Size(100, 31);
+            this.txtBastad2.TabIndex = 8;
+            this.txtBastad2.Text = "52";
+            this.txtBastad2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtBostad1
+            // 
+            this.txtBostad1.Location = new System.Drawing.Point(388, 135);
+            this.txtBostad1.Name = "txtBostad1";
+            this.txtBostad1.Size = new System.Drawing.Size(100, 31);
+            this.txtBostad1.TabIndex = 7;
+            this.txtBostad1.Text = "91";
+            this.txtBostad1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtTrp
+            // 
+            this.txtTrp.Location = new System.Drawing.Point(191, 95);
+            this.txtTrp.Name = "txtTrp";
+            this.txtTrp.Size = new System.Drawing.Size(100, 31);
+            this.txtTrp.TabIndex = 6;
+            this.txtTrp.Text = "17";
+            this.txtTrp.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(569, 45);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 23);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "EL, ÖVRIGT";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(400, 45);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 23);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "VÄRME";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(165, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "RÖRELSEENERGI";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(16, 187);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "INDUSTRI";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(16, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "BÖSTADER";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(16, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 23);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "TRANSPORT";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.chkBio);
+            this.groupBox2.Controls.Add(this.chkFossila);
+            this.groupBox2.Controls.Add(this.chkSpill);
+            this.groupBox2.Controls.Add(this.chkKarn);
+            this.groupBox2.Controls.Add(this.chkVatten);
+            this.groupBox2.Controls.Add(this.chkVind);
+            this.groupBox2.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(938, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(375, 217);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Primär Energi Tillförsel";
+            // 
+            // chkBio
+            // 
+            this.chkBio.AutoSize = true;
+            this.chkBio.Checked = true;
+            this.chkBio.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkBio.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkBio.Location = new System.Drawing.Point(19, 176);
+            this.chkBio.Name = "chkBio";
+            this.chkBio.Size = new System.Drawing.Size(136, 26);
+            this.chkBio.TabIndex = 5;
+            this.chkBio.Text = "Bio Bränslen";
+            this.chkBio.UseVisualStyleBackColor = true;
+            // 
+            // chkFossila
+            // 
+            this.chkFossila.AutoSize = true;
+            this.chkFossila.Checked = true;
+            this.chkFossila.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkFossila.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkFossila.Location = new System.Drawing.Point(19, 149);
+            this.chkFossila.Name = "chkFossila";
+            this.chkFossila.Size = new System.Drawing.Size(163, 26);
+            this.chkFossila.TabIndex = 4;
+            this.chkFossila.Text = "Fossila Bränslen";
+            this.chkFossila.UseVisualStyleBackColor = true;
+            // 
+            // chkSpill
+            // 
+            this.chkSpill.AutoSize = true;
+            this.chkSpill.Checked = true;
+            this.chkSpill.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkSpill.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkSpill.Location = new System.Drawing.Point(19, 122);
+            this.chkSpill.Name = "chkSpill";
+            this.chkSpill.Size = new System.Drawing.Size(67, 26);
+            this.chkSpill.TabIndex = 3;
+            this.chkSpill.Text = "Spill";
+            this.chkSpill.UseVisualStyleBackColor = true;
+            // 
+            // chkKarn
+            // 
+            this.chkKarn.AutoSize = true;
+            this.chkKarn.Checked = true;
+            this.chkKarn.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkKarn.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkKarn.Location = new System.Drawing.Point(19, 95);
+            this.chkKarn.Name = "chkKarn";
+            this.chkKarn.Size = new System.Drawing.Size(72, 26);
+            this.chkKarn.TabIndex = 2;
+            this.chkKarn.Text = "Kärn";
+            this.chkKarn.UseVisualStyleBackColor = true;
+            // 
+            // chkVatten
+            // 
+            this.chkVatten.AutoSize = true;
+            this.chkVatten.Checked = true;
+            this.chkVatten.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkVatten.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkVatten.Location = new System.Drawing.Point(19, 68);
+            this.chkVatten.Name = "chkVatten";
+            this.chkVatten.Size = new System.Drawing.Size(83, 26);
+            this.chkVatten.TabIndex = 1;
+            this.chkVatten.Text = "Vatten";
+            this.chkVatten.UseVisualStyleBackColor = true;
+            // 
+            // chkVind
+            // 
+            this.chkVind.AutoSize = true;
+            this.chkVind.Checked = true;
+            this.chkVind.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkVind.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkVind.Location = new System.Drawing.Point(19, 41);
+            this.chkVind.Name = "chkVind";
+            this.chkVind.Size = new System.Drawing.Size(70, 26);
+            this.chkVind.TabIndex = 0;
+            this.chkVind.Text = "Vind";
+            this.chkVind.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.cboTime);
+            this.groupBox3.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(938, 247);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(375, 100);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Tidsperiod från 2015";
+            // 
+            // cboTime
+            // 
+            this.cboTime.FormattingEnabled = true;
+            this.cboTime.Location = new System.Drawing.Point(19, 36);
+            this.cboTime.Name = "cboTime";
+            this.cboTime.Size = new System.Drawing.Size(92, 31);
+            this.cboTime.TabIndex = 0;
+            this.cboTime.Text = "2015";
+            this.cboTime.SelectedIndexChanged += new System.EventHandler(this.cboTime_SelectedIndexChanged);
+            // 
+            // frmStart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1325, 748);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.statusStrip1);
+            this.MaximizeBox = false;
+            this.Name = "frmStart";
+            this.Text = "frmStart";
+            this.Load += new System.EventHandler(this.frmStart_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.CheckBox chkSpill;
+        private System.Windows.Forms.CheckBox chkKarn;
+        private System.Windows.Forms.CheckBox chkVatten;
+        private System.Windows.Forms.CheckBox chkVind;
+        private System.Windows.Forms.ComboBox cboTime;
+        private System.Windows.Forms.CheckBox chkBio;
+        private System.Windows.Forms.CheckBox chkFossila;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtIndustri2;
+        private System.Windows.Forms.TextBox txtIndustri1;
+        private System.Windows.Forms.TextBox txtBastad2;
+        private System.Windows.Forms.TextBox txtBostad1;
+        private System.Windows.Forms.TextBox txtTrp;
+    }
+}
---
+++ ENM155/frmStart.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ENM155
+{
+    public partial class frmStart : Form
+    {
+
+        #region Local Varables
+
+        private int baseYear = 2015;
+
+        private int elect_housing;
+        private int elect_industry;
+
+        private double transportEnergyFromFossilFuels;
+        private double transportEnergyFromBiogas; 
+
+
+        #endregion
+
+
+        public frmStart()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Text = "KURS ENM155, LP2 : 2017 -> MODELLERING AV SVENSKA ENERGISCENARIER";
+            this.MaximizeBox = false;
+        }
+
+        private void frmStart_Load(object sender, EventArgs e)
+        {
+            //Load all years from now until 2050
+            for (int k = 0; k <= 35; k++)
+            {
+                int formulatedYear = baseYear + k;
+                cboTime.Items.Add(formulatedYear.ToString());
+            }
+
+            CalculateTransportEnergyRelativeBaseYear(2015);
+        }
+
+        #region Transport Calculations
+
+        private double GetTransportEnergy(double overallEnergyUsage, 
+                double FinalEnergyUsage, double fuelEfficiencyProduction, double fuelEfficiencyAfterUsage)
+        {
+            return (overallEnergyUsage * FinalEnergyUsage) / (fuelEfficiencyAfterUsage * fuelEfficiencyProduction);
+        }
+
+        private void CalculateTransportEnergyRelativeBaseYear(int year)
+        {
+            int yearsDifference = (year == baseYear ? 1 : year - baseYear);
+            double incrementFactor = (year == baseYear ? 1 : (0.005 * yearsDifference));
+
+            transportEnergyFromFossilFuels =  GetTransportEnergy(17.0, 0.84, 0.9, 0.2) * incrementFactor;
+            transportEnergyFromBiogas =  GetTransportEnergy(17.0, 0.16, 0.5, 0.2) * incrementFactor;
+        }
+
+        #endregion
+
+        #region Housing Calculations
+
+        #endregion
+
+        #region Industry Calculations
+
+        #endregion
+
+        private void cboTime_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CalculateTransportEnergyRelativeBaseYear(Convert.ToInt32(cboTime.SelectedItem.ToString()));
+            MessageBox.Show((transportEnergyFromFossilFuels + transportEnergyFromBiogas).ToString());
+        }
+    }
+}
